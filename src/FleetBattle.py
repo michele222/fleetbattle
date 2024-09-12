2fd811a4edb75c814d7e77dae91b3262323f7975
unknown <footballwhitestars@gmail.com>
2024-09-12 15:11:58 +0200

    comments and refactor

--- src/FleetBattle.py
+++ src/FleetBattle.py
@@ -5,24 +5,26 @@ from src.Graphics import Graphics
 from src.Ship import Ship
 from src.AttackProbabilityMatrix import AttackProbabilityMatrix
 
+#main class running the game
 class FleetBattle:
     
     def __init__(self):
         self.graphics = Graphics()
-        self.Ships = []
-        self.ShipsEnemy = []
-        self.playerPositions = []
-        self.enemyPositions = []
-        self.phase = 1
+        self.Ships = []             #player ships
+        self.ShipsEnemy = []        #enemy ships
+        self.playerPositions = []   #positions occupied by player ships
+        self.enemyPositions = []    #positions occupied by enemy ships
+        self.phase = 1              #game phase
         for i in Parameters.SHIPS:
             self.Ships.append(Ship(i, self.graphics.getSpaceForShipSize(i)))
             self.ShipsEnemy.append(Ship(i))
-
+    
+    #manual placement of ships by player
     def placeShipsPlayer(self):
         if self.phase == 0:
             return
         placedShips = 0
-        holdShip = -1
+        holdShip = -1 #no ship selected: -1, ship selected: ship id
         #ship placement phase
         while self.phase == 1:
             self.graphics.clearScreen()
@@ -31,30 +33,41 @@ class FleetBattle:
                 self.graphics.drawShip(ship)
 
             mouseKey = pygame.mouse.get_pressed()
-            if mouseKey[0]: #left click mouse
+            if mouseKey[0]: #left click
                 mousePos = pygame.mouse.get_pos()
-                if holdShip < 0: #if no ship is already selected...
+                #ship is selected with mouse
+                if holdShip < 0:
                     for i in range(len(self.Ships)):
-                        if self.Ships[i].body.collidepoint(mousePos): #...and i click on a ship...
-                            holdShip = i   #...ship is now selected
+                        if self.Ships[i].body.collidepoint(mousePos):
+                            holdShip = i
+                #selected ship follows mouse
                 if holdShip >= 0:
-                    self.Ships[holdShip].body.center = mousePos  #ship follows mouse
-            elif holdShip >= 0: #no left click on mouse and ship selected = i just dropped a ship somewhere
-                if self.graphics.isShipInPlacementGrid(self.Ships[holdShip].body):
-                    self.Ships[holdShip].body = self.graphics.alignToPlacementGrid(self.Ships[holdShip].body)
-                    if self.Ships[holdShip].Positions[0] < 0: #i'm moving a ship that was not yet placed in grid
+                    self.Ships[holdShip].body.center = mousePos
+            #selected ship has been released by mouse
+            elif holdShip >= 0:
+                #selected ship has been released inside the placement area
+                if self.graphics.isShipInPlacementArea(self.Ships[holdShip].body):
+                    self.Ships[holdShip].body = self.graphics.alignToPlayerGrid(self.Ships[holdShip].body)
+                    #this is a ship not placed before
+                    if self.Ships[holdShip].Positions[0] < 0:
                         placedShips += 1
                     gridPosition = self.graphics.getGridPositionFromBody(self.Ships[holdShip].body)
                     self.Ships[holdShip].place(gridPosition)
+                    #check collisions with other ships
                     for i in range(len(self.Ships)):
+                        #in case of collision, the ship is placed back in its default position outside of the grid
                         if i != holdShip and any(j in self.Ships[holdShip].Positions for j in self.Ships[i].Positions):
                            self.Ships[holdShip].reset() 
                            placedShips -= 1
                            break           
-                else: #dropped ship outside of placement grid
+                else: #ship released outside of placement grid
+                    #this is a ship that was previously placed
+                    if self.Ships[holdShip].Positions[0] >= 0:
+                        placedShips -= 1
                     self.Ships[holdShip].reset()
                 holdShip = -1
-        
+            
+            #all ships are placed
             if placedShips == len(self.Ships):
                 self.phase = 2
                 break
@@ -62,7 +75,8 @@ class FleetBattle:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.phase = 0
-                elif event.type == pygame.MOUSEBUTTONUP and event.button == 3 and holdShip >= 0: #right mouse button release on a selected ship -> rotate
+                #right mouse button release on a selected ship rotates the ship
+                elif event.type == pygame.MOUSEBUTTONUP and event.button == 3 and holdShip >= 0:
                     self.Ships[holdShip].rotate()
             
             self.graphics.updateScreen()
@@ -70,7 +84,9 @@ class FleetBattle:
 
         for i in range(len(self.Ships)):
             self.playerPositions.extend(self.Ships[i].Positions)
-            
+    
+    #random placement of ships in grid
+    #@player: defines whether player or enemy ships are randomly placed
     def placeShipsRandomly(self, player = True):
         if self.phase == 0:
             return
@@ -84,12 +100,15 @@ class FleetBattle:
             Ships = self.ShipsEnemy    
             positions = self.enemyPositions
         for i in range(len(Ships)):
-            while placedShips == i:    
+            while placedShips == i:
+                #randomly selects ship orientation
                 if random.randint(0, 1):
                     Ships[i].rotate()
+                #randomly selects ship place in grid
                 if Ships[i].place(random.randint(0, Parameters.N * Parameters.N - 1)):
                     placedShips += 1
-                    for y in range(len(Ships)): #check collision with other ships
+                    #checks collision with other ships
+                    for y in range(len(Ships)):
                         if y != i and any(j in Ships[i].Positions for j in Ships[y].Positions):
                             Ships[i].reset() 
                             placedShips -= 1
@@ -99,23 +118,26 @@ class FleetBattle:
             else:        
                 Ships[i].anchorTo(self.graphics.getEnemyGrid())
             positions.extend(Ships[i].Positions)
-
+    
+    #main game phase
     def playMainGamePhase(self):            
         if self.phase == 0:
             return
-        playerAttacks = []
-        enemyAttacks = []
+        playerAttacks = []  #list of positions attacked by player
+        enemyAttacks = []   #list of positions attacked by enemy
         playerHits = 0
         enemyHits = 0
         enemyAttackProbabilities = AttackProbabilityMatrix(Parameters.N, Parameters.N)
+        #main game loop
         while self.phase > 0:
             self.graphics.clearScreen()
             self.graphics.drawPlayerGrid()
             self.graphics.drawEnemyGrid()
             for ship in self.Ships:
                 self.graphics.drawShip(ship)
+            #if game is finished, draw also enemy ships
             if self.phase in (3, 4):
-                for ship in self.ShipsEnemy: #draw also enemy ships if game is finished
+                for ship in self.ShipsEnemy:
                     self.graphics.drawShip(ship)
             for attack in playerAttacks:
                 self.graphics.drawHit(True, attack, attack in self.enemyPositions)
@@ -126,31 +148,32 @@ class FleetBattle:
                 if event.type == pygame.QUIT:
                     self.phase = 0
                 elif event.type == pygame.MOUSEBUTTONUP:
-                    if self.phase > 2: #i clicked after getting game results
+                    #mouse clicked after getting game results
+                    if self.phase > 2: 
                         self.phase = 0
                     else:
-                        mousePos = pygame.mouse.get_pos()
-                        if self.graphics.playerAttacked(mousePos):
-                            attackPos = self.graphics.getAttackPosition(mousePos)
-                            if attackPos not in playerAttacks:
-                                playerAttacks.append(attackPos)
-                                if attackPos in self.enemyPositions:
-                                    playerHits += 1
-                                    if playerHits == len(self.enemyPositions): #WON
-                                        self.phase = 3
-                                if self.phase <= 2:
-                                    while True:    
-                                        attackPos = enemyAttackProbabilities.getNextAttack()
-                                        if attackPos not in enemyAttacks: 
-                                            enemyAttacks.append(attackPos)
-                                            break
-                                    if attackPos in self.playerPositions: #hit
-                                        enemyHits += 1
-                                        enemyAttackProbabilities.update(attackPos, -1)
-                                        if enemyHits == len(self.playerPositions): #LOST
-                                            self.phase = 4
-                                    else: #miss
-                                        enemyAttackProbabilities.update(attackPos, 0)
+                        #handles an attack by the player
+                        attackPos = self.graphics.getAttackPosition()
+                        if attackPos >= 0 and attackPos not in playerAttacks:
+                            playerAttacks.append(attackPos)
+                            if attackPos in self.enemyPositions:
+                                playerHits += 1 #enemy ship hit by player
+                                if playerHits == len(self.enemyPositions):
+                                    self.phase = 3 #player wins
+                            if self.phase <= 2:
+                                while True:    
+                                    #handles an attack by the enemy
+                                    attackPos = enemyAttackProbabilities.getNextAttack()
+                                    if attackPos not in enemyAttacks: 
+                                        enemyAttacks.append(attackPos)
+                                        break
+                                if attackPos in self.playerPositions:
+                                    enemyHits += 1 #player ship hit by enemy
+                                    enemyAttackProbabilities.update(attackPos, -1)
+                                    if enemyHits == len(self.playerPositions):
+                                        self.phase = 4 #enemy wins
+                                else: #miss
+                                    enemyAttackProbabilities.update(attackPos, 0)
             
             match self.phase:
                 case 1 | 2:
--- src/Graphics.py
+++ src/Graphics.py
@@ -1,7 +1,7 @@
 import pygame
 import Parameters
-import Ship
 
+#class handling all graphic elements of the game
 class Graphics:
     
     def __init__(self):
@@ -9,83 +9,136 @@ class Graphics:
         self.screen = pygame.display.set_mode((Parameters.WIDTH, Parameters.HEIGHT))
         pygame.display.set_caption('FleetBattle')
         self.clock = pygame.time.Clock()
-        self.placementGrid = pygame.Rect((0, 0, 2 * Parameters.MARGIN + Parameters.N * Parameters.SQUARE, 2 * Parameters.MARGIN + Parameters.N * Parameters.SQUARE))
-        self.playerGrid = pygame.Rect((Parameters.MARGIN, Parameters.MARGIN, Parameters.N * Parameters.SQUARE, Parameters.N * Parameters.SQUARE))
-        self.attackGrid = pygame.Rect((2 * Parameters.MARGIN + Parameters.N * Parameters.SQUARE, Parameters.MARGIN, Parameters.N * Parameters.SQUARE, Parameters.N * Parameters.SQUARE))
+        #the grid showing player ships
+        self.playerGrid = pygame.Rect((Parameters.MARGIN,
+                                       Parameters.MARGIN,
+                                       Parameters.N * Parameters.SQUARE,
+                                       Parameters.N * Parameters.SQUARE))
+        #the grid showing player attacks
+        self.attackGrid = pygame.Rect((2 * Parameters.MARGIN + Parameters.N * Parameters.SQUARE,
+                                       Parameters.MARGIN,
+                                       Parameters.N * Parameters.SQUARE,
+                                       Parameters.N * Parameters.SQUARE))
+        #the area where ships are placed by the player. Equals the playerGrid with margin around
+        self.placementArea = pygame.Rect((self.playerGrid.topleft[0] - Parameters.MARGIN,
+                                          self.playerGrid.topleft[1] - Parameters.MARGIN,
+                                          Parameters.N * Parameters.SQUARE + 2 * Parameters.MARGIN,
+                                          Parameters.N * Parameters.SQUARE + 2 * Parameters.MARGIN))
+        #before placement, ships are initially drawn in an invisible grid starting with position placementPos
         self.placementPos = 0
+        #background image
         self.bgTexture = pygame.image.load(r'assets\bg.png').convert_alpha()
-        self.bgTexture = pygame.transform.smoothscale(self.bgTexture, (Parameters.N * Parameters.SQUARE, Parameters.N * Parameters.SQUARE))
+        self.bgTexture = pygame.transform.smoothscale(self.bgTexture,
+                                                      (Parameters.N * Parameters.SQUARE,
+                                                       Parameters.N * Parameters.SQUARE))
+        #ship images are initialized based on ship length
         self.ShipTextures = {}
         for i in range (1, 6):
             self.ShipTextures[i] = pygame.image.load(f'assets\ship{i}.png').convert_alpha()
-            self.ShipTextures[i] = pygame.transform.smoothscale(self.ShipTextures[i], (i * Parameters.SQUARE, Parameters.SQUARE))
+            self.ShipTextures[i] = pygame.transform.smoothscale(self.ShipTextures[i],
+                                                                (i * Parameters.SQUARE,
+                                                                 Parameters.SQUARE))
+        #explosion images
         self.HitTexture = {
-            True : pygame.image.load('assets\explosion.png').convert_alpha(),
-            False : pygame.image.load('assets\explosion_sea.png').convert_alpha()
+            True : pygame.image.load('assets\explosion.png').convert_alpha(),       #True: explosion on ship (hit)
+            False : pygame.image.load('assets\explosion_sea.png').convert_alpha()   #False: explosion at sea (miss)
         }
         for i in self.HitTexture:
-            self.HitTexture[i] = pygame.transform.smoothscale(self.HitTexture[i], (Parameters.SQUARE, Parameters.SQUARE))
+            self.HitTexture[i] = pygame.transform.smoothscale(self.HitTexture[i],
+                                                              (Parameters.SQUARE,
+                                                               Parameters.SQUARE))
     
     def __del__(self):
         pygame.quit()
+    
+    def clearScreen(self):
+        self.screen.fill((0, 0, 0))
+    
+    #draws all objects on screen
+    def updateScreen(self):
+        pygame.display.flip()
 
-    def drawGrid(self, offset = (0, 0)): #draw placement grid
+    #draws a N*N grid with background image
+    #@offset: topleft point of the grid    
+    def drawGrid(self, offset = (0, 0)):
         self.screen.blit(self.bgTexture, offset)
         for i in range(Parameters.N+1):
-            pygame.draw.line(self.screen, (255, 255, 255), (offset[0], offset[1] + i * Parameters.SQUARE), (offset[0] + Parameters.N * Parameters.SQUARE, offset[1] + i * Parameters.SQUARE))
-            pygame.draw.line(self.screen, (255, 255, 255), (offset[0] + i * Parameters.SQUARE, offset[1]), (offset[0] + i * Parameters.SQUARE, offset[1] + Parameters.N * Parameters.SQUARE))
-        
+            #horizontal lines
+            pygame.draw.line(self.screen,
+                             (255, 255, 255),
+                             (offset[0],
+                              offset[1] + i * Parameters.SQUARE),
+                             (offset[0] + Parameters.N * Parameters.SQUARE,
+                              offset[1] + i * Parameters.SQUARE))
+            #vertical lines
+            pygame.draw.line(self.screen,
+                             (255, 255, 255),
+                             (offset[0] + i * Parameters.SQUARE,
+                              offset[1]),
+                             (offset[0] + i * Parameters.SQUARE,
+                              offset[1] + Parameters.N * Parameters.SQUARE))
+    
     def drawPlayerGrid(self):
         self.drawGrid(self.playerGrid.topleft)
         
     def drawEnemyGrid(self):
         self.drawGrid(self.attackGrid.topleft)
 
+    #draws a hit image (explosion)
+    #@isPlayer: True when player attacks, False when enemy attacks     
+    #@pos: position of the attack on grid
+    #@hit: True when hit, False when miss    
     def drawHit(self, isPlayer, pos, hit):
         offset = self.playerGrid.topleft
-        if isPlayer is True:
+        if isPlayer:
             offset = self.attackGrid.topleft
-        color = (0, 0, 255)
-        if hit:
-            color = (255, 0, 0)
-        #pygame.draw.rect(self.screen, color, (offset[0] + Parameters.SQUARE * (pos % Parameters.N), offset[1] + Parameters.SQUARE * (pos // Parameters.N), Parameters.SQUARE, Parameters.SQUARE))
-        self.screen.blit(self.HitTexture[hit], (offset[0] + Parameters.SQUARE * (pos % Parameters.N), offset[1] + Parameters.SQUARE * (pos // Parameters.N)))
+        self.screen.blit(self.HitTexture[hit],
+                         (offset[0] + Parameters.SQUARE * (pos % Parameters.N),
+                          offset[1] + Parameters.SQUARE * (pos // Parameters.N)))
     
-    def clearScreen(self):
-        self.screen.fill((0, 0, 0))
-        
-    def updateScreen(self):
-        pygame.display.flip()
-        
+    #draws a ship image
+    #@ship: object ship    
     def drawShip(self, ship):
-        #pygame.draw.rect(self.screen, (0, 255, 0), ship.body)
         if ship.horizontal:
-            self.screen.blit(self.ShipTextures[ship.length], ship.body.topleft)
+            self.screen.blit(self.ShipTextures[ship.length],
+                             ship.body.topleft)
         else:
-            self.screen.blit(pygame.transform.rotate(self.ShipTextures[ship.length], -90), ship.body.topleft)
-        
-    def playerAttacked(self, mousePos):
+            self.screen.blit(pygame.transform.rotate(self.ShipTextures[ship.length],
+                                                     -90),
+                             ship.body.topleft)
+    
+    #gets attack position based on mouse position
+    #return: position in attack grid, -1 if outside grid    
+    def getAttackPosition(self):
+        mousePos = pygame.mouse.get_pos()
         if self.attackGrid.collidepoint(mousePos):
-            return True
+            return ((mousePos[0] - self.attackGrid.x) // Parameters.SQUARE
+                    + ((mousePos[1] - self.attackGrid.y) // Parameters.SQUARE) * Parameters.N)
         else:
-            return False
+            return -1   
         
-    def getAttackPosition(self, mousePos):
-        return (mousePos[0] - self.attackGrid.x) // Parameters.SQUARE + ((mousePos[1] - self.attackGrid.y) // Parameters.SQUARE) * Parameters.N
+    #gets grid position from ship rectangle
+    #@body: ship body rectangle
+    #return: grid position 
+    def getGridPositionFromBody(self, body):
+        return ((body.x - self.playerGrid.left) // Parameters.SQUARE
+                + (body.y - self.playerGrid.top) * Parameters.N // Parameters.SQUARE)
     
-    def isShipInPlacementGrid(self, body):
-        if self.placementGrid.contains(body):
+    #checks whether a ship rectangle is fully in the placement area
+    #@body: ship body rectangle 
+    def isShipInPlacementArea(self, body):
+        if self.placementArea.contains(body):
             return True
         else:
             return False
     
-    def alignToPlacementGrid(self, body):
-        body.x = round((body.x - Parameters.MARGIN) / Parameters.SQUARE) * Parameters.SQUARE + Parameters.MARGIN
-        body.y = round((body.y - Parameters.MARGIN) / Parameters.SQUARE) * Parameters.SQUARE + Parameters.MARGIN
+    #aligns a ship rectangle to the placement grid
+    #@body: ship body rectangle 
+    #return: aligned ship body rectangle    
+    def alignToPlayerGrid(self, body):
+        body.x = round((body.x - self.playerGrid.left) / Parameters.SQUARE) * Parameters.SQUARE + self.playerGrid.left
+        body.y = round((body.y - self.playerGrid.top) / Parameters.SQUARE) * Parameters.SQUARE + self.playerGrid.top
         return body
-
-    def getGridPositionFromBody(self, body):
-        return (body.x - Parameters.MARGIN) // Parameters.SQUARE + (body.y - Parameters.MARGIN) * Parameters.N // Parameters.SQUARE
     
     def getPlayerGrid(self):
         return self.playerGrid.topleft
@@ -93,16 +146,29 @@ class Graphics:
     def getEnemyGrid(self):
         return self.attackGrid.topleft
 
+    #assigns the next available spot for a ship for the initial ship drawing phase
+    #ships are drawn in the attack grid with extra margins, the grid itself is not drawn in this phase
+    #@size: size of the ship to be placed
+    #return: next available position (x, y) that can fit the ship
     def getSpaceForShipSize(self, size):
+        #if the ship doesn't fit in the current line, move to the leftmost position one line down
         if self.placementPos // Parameters.N != (self.placementPos + size) // Parameters.N:
             self.placementPos = (self.placementPos // Parameters.N + 1) * Parameters.N
-        ret = (self.attackGrid.topleft[0] + Parameters.SQUARE * (self.placementPos % Parameters.N), self.attackGrid.topleft[1] + (Parameters.SQUARE + Parameters.MARGIN) * (self.placementPos // Parameters.N))
+        ret = (self.attackGrid.topleft[0]
+               + Parameters.SQUARE
+               * (self.placementPos % Parameters.N),
+               self.attackGrid.topleft[1]
+               + (Parameters.SQUARE + Parameters.MARGIN)
+               * (self.placementPos // Parameters.N))
         self.placementPos += (size + 1)         
         return ret
-            
+    
+    #displays a message in the center of the screen, below the grids
+    #@message: the message to be displayed
     def textWindow(self, message):
         font = pygame.font.Font('freesansbold.ttf', 32)
         text = font.render(message, True, (255, 255, 255))
         textRect = text.get_rect()
-        textRect.midtop = (Parameters.WIDTH // 2, self.playerGrid.bottomright[0] + Parameters.MARGIN // 2)
+        textRect.midtop = (Parameters.WIDTH // 2,
+                           self.playerGrid.bottomright[0] + Parameters.MARGIN // 2)
         self.screen.blit(text, textRect)
